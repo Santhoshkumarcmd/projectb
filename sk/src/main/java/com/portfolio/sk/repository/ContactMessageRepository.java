f109459dae55b3e01183187fc7ff5826ad2e2c01
Santhosh Kumar S <sk0661527@gmail.com>
2025-06-29 08:43:55 +0530

    Clean up project structure and add all files

--- sk/src/main/java/com/portfolio/sk/repository/ContactMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.sk.repository;
-
-import com.portfolio.sk.entity.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
-}
--- sk/src/main/java/com/portfolio/sk/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.sk.repository;
-
-import com.portfolio.sk.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
